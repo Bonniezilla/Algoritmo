1bdedf3fa39bb62b2406dd3cb3a0c19af048cf11
Bonniezilla <bonnieevil86@gmail.com>
2023-02-27 07:44:10 -0300

    LEIA O README

---
+++ verificaZero.js
@@ -0,0 +1,11 @@
+var n1;
+
+function verificaZero() {
+    if (n1 < 0) {
+        console.log("O número é negativo");
+    } else if (n1 > 0) {
+        console.log("O número é posítivo");
+    } else {
+        console.log("O número deve ser diferente de zero");
+    }
+}
